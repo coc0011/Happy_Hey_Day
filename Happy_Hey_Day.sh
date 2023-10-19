7b86eb8ee4c4068f6f3247788f9d408dd7f0d3e8
coc0011 <145736238+coc0011@users.noreply.github.com>
2023-10-18 20:56:12 -0500

    Add files via upload

---
+++ Happy_Hey_Day.sh
@@ -0,0 +1,10 @@
+#! bin/bash
+
+#ask the user to enter their name
+echo "What is your name?"
+
+#instruct the computer to interpret the input as a variable called name
+read name
+
+#print this statement back to the user using the name variable
+echo "Hey, $name!"
